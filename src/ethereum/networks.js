b98490ce970a30c6a3d84cd6c1e409a3135659ab
Thomas Viana <thomasviana16@gmail.com>
2022-06-06 10:37:16 -0500

    Add network and contract abi files.

---
+++ src/ethereum/networks.js
@@ -0,0 +1,9 @@
+export default {
+  "4": {
+    events: {},
+    links: {},
+    address: "0xD1Af604D0856e2cE9Af44d453F147a9B983fca6f",
+    transactionHash:
+      "0xed45b892636a91cce7ea6a1cb17b0a864471bb8db3e45260dcccd779f5d04b7a ",
+  },
+};
